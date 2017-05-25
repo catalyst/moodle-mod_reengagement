53d61cc11e9474bb50527e8c3809d9de19a101b5
Dan Marsden <dan@danmarsden.com>
2017-05-26 09:23:30 +1200

    Fixes #25

--- mod_form.php
+++ mod_form.php
@@ -182,8 +182,16 @@ class mod_reengagement_mod_form extends moodleform_mod {
             $mform->setDefault('completion', COMPLETION_TRACKING_AUTOMATIC);
             $mform->freeze('completion');
         }
+        // Hide some elements not relevant to this activity (student visibility) 
         if ($mform->elementExists('visible')) {
             $mform->removeElement('visible');
+            $mform->addElement('hidden', 'visible', 0);
+            $mform->setType('visible', PARAM_INT);
+            if ($mform->elementExists('visibleoncoursepage')) {
+                $mform->removeElement('visibleoncoursepage');
+            }
+            $mform->addElement('hidden', 'visibleoncoursepage', 1);
+            $mform->setType('visibleoncoursepage', PARAM_INT);
         }
 
         // Add standard buttons, common to all modules.
