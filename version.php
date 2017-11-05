4785d7adedfe07f852d13977968e8023010d1130
Dan Marsden <dan@danmarsden.com>
2017-11-06 09:18:33 +1300

    bump version for plugins db release.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017102001;   // The current module version.
+$plugin->version   = 2017102002;   // The current module version.
 $plugin->requires  = 2016082500;
 $plugin->component = 'mod_reengagement';
-$plugin->release   = '3.3.4';
+$plugin->release   = '3.3.5';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0; // Now uses a scheduled task.
