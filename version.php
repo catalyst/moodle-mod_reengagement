969774141849c0245e638b2d382704f9d2a556e8
Dan Marsden <dan@danmarsden.com>
2018-01-17 16:39:50 +1300

    requires Moodle 3.4 to use hideif().

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018011700;   // The current module version.
-$plugin->requires  = 2016082500;
+$plugin->requires  = 2017111300;
 $plugin->component = 'mod_reengagement';
 $plugin->release   = '3.3.5';
 $plugin->maturity  = MATURITY_STABLE;
