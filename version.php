08a662f9c905efeb286980a6876a1cf62ab1a832
Dan Marsden <dan@danmarsden.com>
2018-04-05 10:01:25 +1200

    bump version for plugins db.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017050802;   // The current module version.
+$plugin->version   = 2017050803;   // The current module version.
 $plugin->requires  = 2016082500;
 $plugin->component = 'mod_reengagement';
 $plugin->release   = '3.3.1';
