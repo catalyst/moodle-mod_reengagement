c62d643eba6ecd9b9bb9152b920066f12c53a75a
Nathan Nguyen <nathannguyen@catalyst-au.net>
2019-04-23 09:55:13 +1000

    WR#310004 - test generator does not cover not null field

--- tests/generator/lib.php
+++ tests/generator/lib.php
@@ -36,6 +36,57 @@ defined('MOODLE_INTERNAL') || die();
 class mod_reengagement_generator extends testing_module_generator {
 
     public function create_instance($record = null, array $options = null) {
+        $record = (object)(array)$record;
+
+        if (!isset($record->name)) {
+            $record->name = 'Test Reengagement';
+        }
+        if (!isset($record->timecreated)) {
+            $record->timecreated = time();
+        }
+        if (!isset($record->timemodified)) {
+            $record->timemodified = time();
+        }
+        if (!isset($record->emailuser)) {
+            $record->emailuser = 1;
+        }
+        if (!isset($record->emailcontent)) {
+            $record->emailcontent = 'Email Content';
+        }
+        if (!isset($record->emailcontentformat)) {
+            $record->emailcontentformat = 0;
+        }
+        if (!isset($record->emailsubject)) {
+            $record->emailsubject = 'Email Subject';
+        }
+        if (!isset($record->emailcontentmanager)) {
+            $record->emailcontentmanager = 'Email Content Manager';
+        }
+        if (!isset($record->emailcontentmanagerformat)) {
+            $record->emailcontentmanagerformat = 0;
+        }
+        if (!isset($record->emailsubjectmanager)) {
+            $record->emailsubjectmanager = 'Email Subject Manager';
+        }
+        if (!isset($record->emailrecipient)) {
+            $record->emailrecipient = 0;
+        }
+        if (!isset($record->duration)) {
+            $record->duration = 604800;
+        }
+        if (!isset($record->remindercount)) {
+            $record->remindercount = 1;
+        }
+        if (!isset($record->suppresstarget)) {
+            $record->suppresstarget = 0;
+        }
+        if (!isset($record->emaildelay)) {
+            $record->emaildelay = 604800;
+        }
+        if (!isset($record->suppressemail)) {
+            $record->suppressemail = 0;
+        }
+
         return parent::create_instance($record, $options);
     }
 }
