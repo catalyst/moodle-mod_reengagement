767e10af3dc9d0d6fce2afda4983b4d75f00a503
Peter Burnett <peterburnett@catalyst-au.net>
2020-07-09 13:21:24 +1000

    Issue #76: Added activity description text

--- lang/en/reengagement.php
+++ lang/en/reengagement.php
@@ -28,6 +28,21 @@ $string['reengagement'] = 'reengagement';
 $string['pluginadministration'] = '';
 $string['modulename'] = 'Reengagement';
 $string['modulenameplural'] = 'Reengagements';
+$string['modulename_help'] = 'The Reengagement activity provides a way for you to send timely notifications to students and 3rd parties, which can include reminders to return to the course and complete activities.
+
+The notification trigger is a specified period of time after a starting event, which may be enrolment in the course, completing an activity, or other course variables.
+Notifications can be set to repeat for a limited run, and/or until the target activity is completed, and can have delay periods between repeats.
+If the user has already completed the target activity, no email is sent.
+
+You could use this plugin to: <ul>
+<li>Selectively release timed content in a course.</li>
+<li>Repeatedly remind students that an assignment is overdue.</li>
+<li>Advise training managers that the course is complete.</li>
+</ul>
+
+The Reengagement activity is very flexible, explore what you can do with it.
+
+<a href=https://docs.moodle.org/39/en/Reengagement_activity>More Help</a>';
 
 // Alphabetized.
 $string['activitycompleted'] = 'This activity has been marked as complete';
@@ -121,4 +136,3 @@ $string['weeks'] = 'Weeks';
 $string['withselectedusers'] = 'With selected users...';
 $string['withselectedusers_help'] = '* Send message - For sending a message to one or more participants
 * Reset completion date by course access - For adjusting the reengagement completion date based on the first access to this course.';
-
