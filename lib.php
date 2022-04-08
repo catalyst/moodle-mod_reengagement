catalyst/moodle-mod_reengagement

commit 2d3262d14ac0f37bb571794a13f2b6590906ef44

ISSUE-9 Added filter support for subject and content of emails (#113)

lib.php
@@ -621,6 +621,22 @@ function reengagement_template_variables($reengagement, $inprogress, $user) {
     foreach ($replacementfields as $field) {
         $results[$field] = preg_replace($patterns, $replacements, $reengagement->$field);
     }
+
+    // Apply enabled filters to email content.
+    $options = array(
+            'context' => context_course::instance($reengagement->courseid),
+            'noclean' => true,
+            'trusted' => true
+    );
+    $subjectfields = array('emailsubject', 'emailsubjectmanager', 'emailsubjectthirdparty');
+    foreach ($subjectfields as $field) {
+        $results[$field] = format_text($reengagement->$field, FORMAT_PLAIN, $options);
+    }
+    $contentfields = array('emailcontent', 'emailcontentmanager', 'emailcontentthirdparty');
+    foreach ($contentfields as $field) {
+        $results[$field] = format_text($reengagement->$field, FORMAT_MOODLE, $options);
+    }
+
     return $results;
 }
 

commit 8adebe233f7d607b51c4711a25480ffd3f791c43

Fix #118 fix regression caused by #113

lib.php
@@ -631,11 +631,11 @@ function reengagement_template_variables($reengagement, $inprogress, $user) {
     );
     $subjectfields = array('emailsubject', 'emailsubjectmanager', 'emailsubjectthirdparty');
     foreach ($subjectfields as $field) {
-        $results[$field] = format_text($reengagement->$field, FORMAT_PLAIN, $options);
+        $results[$field] = format_text($results[$field], FORMAT_PLAIN, $options);
     }
     $contentfields = array('emailcontent', 'emailcontentmanager', 'emailcontentthirdparty');
     foreach ($contentfields as $field) {
-        $results[$field] = format_text($reengagement->$field, FORMAT_MOODLE, $options);
+        $results[$field] = format_text($results[$field], FORMAT_MOODLE, $options);
     }
 
     return $results;