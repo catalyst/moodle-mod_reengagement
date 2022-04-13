d06e2bbb2a785eb0b16cb6f64ddc86938e3b7f25
Dan Marsden <dan@danmarsden.com>
2022-04-13 12:08:28 +1200

    bump version with bugfixes.

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022031700;   // The current module version.
+$plugin->version   = 2022041300;   // The current module version.
 $plugin->requires  = 2021091700; // Requires 4.0.
 $plugin->component = 'mod_reengagement';
-$plugin->release   = '4.0';
+$plugin->release   = '4.1';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0; // Now uses a scheduled task.
 $plugin->supported = [400, 400];
