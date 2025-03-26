814908a73cf27dda4490009361d854ad68ddd5d8
Sasha Anastasi <sasha.anastasi@catalyst.net.nz>
2025-03-26 13:26:47 +1300

    update version for t17

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018022501;   // The current module version.
-$plugin->requires  = 2016120505; // Totara 10
+$plugin->version   = 2018040501;   // The current module version.
+$plugin->requires  = 2022110800; // Totara 17
 $plugin->component = 'mod_reengagement';
-$plugin->release   = '3.2.1';
+$plugin->release   = '3.5.2';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0; // Now uses a scheduled task.
